9a9305b6f401803a9733c2409c3c8e53067d8883
Vani Kanoria '22 <vkanoria@colgate.edu>
2020-01-24 12:16:55 -0500

    created numbers and vectors and did computations

---
+++ activity1/activity1_script.r
@@ -0,0 +1,18 @@
+#activity 1
+print("hello R world")
+
+#three times three
+3*3
+
+#
+#name my number
+a <- 45768467
+
+#multiply my number by 10
+a*10
+
+#make a vector of numbers
+v <- c(3, 4, 5, 6)
+
+#multiply my vector by my number
+v*a
